ad6b320292b6fa795cfc09954b035b616967f686
Ydeback <niklas.ydeback@gmail.com>
2020-10-07 16:06:20 +0200

    changes for printing data and angles

--- client2.py
+++ client2.py
@@ -1,16 +1,23 @@
 import socket
 import time
+import numpy as np
 
 HOST = "169.254.93.123"
 PORT = 2112
 ADDRESS = (HOST,PORT)
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+def parse_number(data):
+    if b'+' in data or b'-' in data:
+        return int(data)
+    else:
+        return int(data,16)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Socket created")
 
     try:
         s.connect(ADDRESS)
-        print("Connected to:",HOST)
+        print("Connected to:", HOST)
     except socket.error:
         print("Connection failed")
 
@@ -20,8 +27,15 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         s.send(msg)
         data = s.recv(2048)
         print("data received")
-        print(data)
+        
+        data = data.split(b' ')
+        number = parse_number(data[25]) 
+        print(number)   
+        dist = [parse_number(x) / 1000 for x in data[26:26+number]]
+        print(dist)
+        angle = np.linspace(0.08726646259, -0.08726646259, number)
+        print(angle)
         time.sleep(3)
-
+        
 
 
