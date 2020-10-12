7692d8d05230ff5dcf8c4257c642d22027e38d2a
ercmar-6 <ercmar-6@student.ltu.se>
2020-10-12 11:39:45 +0200

    Intervall filter och identifiera kabelavstånd från centrum.

--- client2.py
+++ client2.py
@@ -1,5 +1,5 @@
 import socket
-import time
+import math
 import numpy as np
 
 HOST = "169.254.93.123"
@@ -10,7 +10,18 @@ def parse_number(data):
     if b'+' in data or b'-' in data:
         return int(data)
     else:
-        return int(data,16)
+        return int(data, 16)
+
+def layercheck(data):
+    if data == b'FE0C':
+        print("Layer: 4")
+    elif data == b'FF06':
+        print("Layer: 3")
+    elif data == b'0':
+        print("Layer: 2")
+    elif data == b'FA':
+        print("Layer: 1")
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Socket created")
@@ -21,21 +32,43 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     except socket.error:
         print("Connection failed")
 
-    msg = b'\x02sRN LMDscandata 1\x03\0'
-
+    msg = b'\x02sEN LMDscandata 1\x03\0'
+    s.send(msg)
     while True:
-        s.send(msg)
+
         data = s.recv(2048)
-        print("data received")
-        
-        data = data.split(b' ')
-        number = parse_number(data[25]) 
-        print(number)   
-        dist = [parse_number(x) / 1000 for x in data[26:26+number]]
-        print(dist)
-        angle = np.linspace(0.08726646259, -0.08726646259, number)
-        print(angle)
-        time.sleep(3)
-        
+        print('ASCII:', data)
 
+        if len(data) < 30: #Ignorera första strängen som skickas
+            continue
+        else:
+            data = data.split(b' ')
+            number = parse_number(data[25])
+            layercheck(data[15])
+            print('Number points:', number)
+            dist = [parse_number(x) / 1000 for x in data[26:26+number]]
+            print('Distance [m]:', dist)
 
+            arr = np.array(dist)  #gör om dist listan till array
+
+            #Filter som sätter allt efter ett visst avstånd till 0
+            high_threshold = 2.5
+            low_threshold = 0.5
+
+            arr[arr > high_threshold] = 99
+            arr[arr < low_threshold] = 99
+            print(arr)
+
+            index = np.where(arr == np.amin(arr)) #hitta index för det minimala eller maximala avståndet
+            #print(index)
+            print(arr[index[0]])
+
+            #skala mätpunkterna utifrån vinklarna.
+            angle = np.linspace(0.08726646259, -0.08726646259, number)
+            print(angle[index[0]])
+
+            #Beräkna position i sidled
+            x = np.sin(angle)
+            #print(x[index])
+            pos = np.multiply(x[index],arr[index[0]])
+            print(pos)
