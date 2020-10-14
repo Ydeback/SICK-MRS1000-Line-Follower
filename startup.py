b85a7d29ef281ff06aeb78e79676708e88c2fe2e
Ydeback <niklas.ydeback@gmail.com>
2020-10-14 14:36:14 +0200

    Minor changes

--- startup.py
+++ startup.py
@@ -6,12 +6,16 @@
 
 # Device ip-address
 HOST = "169.254.93.123" 
+
 # Commication port
 PORT = 2112
+
 # Create Address for the device
 ADDRESS = (HOST,PORT) 
+
 # Temporary value of start angle (horz)(rad)
 START_ANGLE = 0.08726646259 
+
 # Temporary value of stop angle (horz)(rad)
 STOP_ANGLE = -0.08726646259 
 
@@ -19,14 +23,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the LiDAR
 def connect(): 
-    
     try:
         s.connect(ADDRESS)
         print("Connected to:", HOST)
     except socket.error as error:
         print("Connection failed: ", error)
-
-    pass
 
 # Loading the config of the MRS1000c LiDAR 
 def load_config(): 
