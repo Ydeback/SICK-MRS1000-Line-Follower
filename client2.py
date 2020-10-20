e0caef353bdd4de24df01f4349aa42a3e2dbae32
ercmar-6 <ercmar-6@student.ltu.se>
2020-10-20 08:43:59 +0200

    created functions(Lengthfilter and analysis) and clarified the prints.

--- client2.py
+++ client2.py
@@ -5,9 +5,10 @@ import numpy as np
 HOST = "169.254.93.123"
 PORT = 2112
 ADDRESS = (HOST,PORT)
-START_ANGLE = 0.08726646259
-STOP_ANGLE = -0.08726646259
+START_ANGLE = 10 * np.pi /180
+STOP_ANGLE = -10 * np.pi /180
 
+layer=0
 def parse_number(data):
     if b'+' in data or b'-' in data:
         return int(data)
@@ -17,12 +18,43 @@ def parse_number(data):
 def layercheck(data):
     if data == b'FE0C':
         print("Layer: 4")
+
     elif data == b'FF06':
         print("Layer: 3")
+
     elif data == b'0':
         print("Layer: 2")
+
     elif data == b'FA':
         print("Layer: 1")
+
+def Lengthfilter(arr):
+    # Filter the distances to only care for the specified interval
+    high_threshold = 2.2
+    low_threshold = 0.5
+
+    arr[arr > high_threshold] = 99
+    arr[arr < low_threshold] = 99
+    print(arr)
+    return arr
+def analysis(arr):
+
+    # Identify both cabels
+    index1 = np.where(arr == np.amin(arr))
+
+    # value of the accepted distances
+    print('Dist1:', arr[index1[0]])
+
+    # Rescale the distances to the measured angles of the LiDAR
+    angle = np.linspace(START_ANGLE, STOP_ANGLE, number)
+    print(angle[index1[0]])
+
+    # Calculate the distance of the object from the centre point of the
+    # LiDAR measurement
+    x = np.sin(angle)
+    pos = np.multiply(x[index1], arr[index1[0]])
+    print(pos)
+    return pos
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -41,36 +73,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data = s.recv(2048)
         print('ASCII:', data)
 
-        if len(data) < 30: # Ignore 30 first data points of the input
+        # Ignore 30 first data points of the input
+        if len(data) < 30:
             continue
         else:
             data = data.split(b' ')
             number = parse_number(data[25])
-            layercheck(data[15])
+
             print('Number points:', number)
             dist = [parse_number(x) / 1000 for x in data[26:26+number]]
             print('Distance [m]:', dist)
 
-            arr = np.array(dist)  # Remake the distances to an array
+            # Remake the distances to an array
+            arr = np.array(dist)
 
-            # Filter the distances to only care for the specified interval
-            high_threshold = 2.5
-            low_threshold = 0.5
+            # Layer filter
+            layercheck(data[15])
 
-            arr[arr > high_threshold] = 99
-            arr[arr < low_threshold] = 99
-            print(arr)
+            # Length filter
+            arr = Lengthfilter(arr)
 
-            index = np.where(arr == np.amin(arr)) #Find the max and/or min
-            # value of the accepted distances
-            print(arr[index[0]])
+            # Get position from centerpoint
+            pos = analysis(arr)
 
-            # Rescale the distances to the measured angles of the LiDAR
-            angle = np.linspace(START_ANGLE, STOP_ANGLE, number)
-            print(angle[index[0]])
-
-            # Calculate the distance of the object from the centre point of the
-            # LiDAR measurement
-            x = np.sin(angle)
-            pos = np.multiply(x[index],arr[index[0]])
-            print(pos)
